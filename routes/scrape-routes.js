9c86eb76b9bf67dd73b0c4b4218d1c02f57d4615
Sujata Jhaveri <sujata.jhaveri@gmail.com>
2018-10-04 15:33:28 -0400

    Changed router to /index

--- routes/scrape-routes.js
+++ routes/scrape-routes.js
@@ -36,7 +36,7 @@ router.get("/api/scrape", function (req, res) {
       });
     });
     console.log("scrape comepleted");
-    res.redirect("/");
+    res.redirect("/index");
 
   });
 });
@@ -102,7 +102,7 @@ router.get("/api/clear/notSaved", function (req, res) {
   console.log("enter clear all");
   db.Article.remove({ saved: false })
     .then(function (dbArticle) {
-      res.redirect('/');
+      res.redirect('/index');
     })
 });
 
@@ -110,7 +110,7 @@ router.get("/api/clear/saved", function (req, res) {
   console.log("enter clear saved");
   db.Article.updateMany({ saved: true }, { $set: { "saved": false } })
     .then(function (dbArticle) {
-      res.redirect('/');
+      res.redirect('/index');
     })
 });
 
