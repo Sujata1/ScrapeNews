02dee756c20ce23ddf13dfddc4d862b9e7e6c727
Sujata Jhaveri <sujata.jhaveri@gmail.com>
2018-10-04 15:17:17 -0400

    Added First version

---
+++ routes/html-routes.js
@@ -0,0 +1,47 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require('path');
+
+// Routes
+// =============================================================
+var express = require('express');
+var router = express.Router();
+var mongoose = require("mongoose");
+var db = require('../models');
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapenews";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// lost route loads lost.html
+
+//Create all our routes and set up logic within those routes where required.
+
+router.get('/', function (req, res) {
+    console.log("enter index in html-router");
+    db.Article.find({ saved: false })
+    .then(function (dbArticle) {
+        res.render("index", {articles: dbArticle});
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+    
+  });
+
+  
+router.get('/savedarticles', function (req, res) {
+  console.log("enter savedarticles in html-router");
+  db.Article.find({ saved: true })
+  .then(function (dbArticle) {
+      res.render("saved", {articles: dbArticle});
+  })
+  .catch(function (err) {
+    res.json(err);
+  });
+  
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var PORT = 3000;
+
+var app = express();
+// Configure middleware
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+// Routes
+
+
+var routes = require("./routes/html-routes");
+var scraproutes = require("./routes/scrape-routes");
+app.use(routes);
+app.use(scraproutes);
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
