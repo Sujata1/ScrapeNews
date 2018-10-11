4e61a610b6e06da7e7e535ae5ad17225f29840b2
Sujata Jhaveri <sujata.jhaveri@gmail.com>
2018-10-11 15:15:58 -0400

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 
-var PORT = 3000;
+var PORT = process.env.port || 3000;
 
 var app = express();
 // Configure middleware
